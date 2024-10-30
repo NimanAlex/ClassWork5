e12b0404fa586bcc8f7c24c3594ad13ea069fd8b
Александр Надеин <sashanadein0907@gmail.com>
2024-10-30 09:33:57 +0100

    Main was changed

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,13 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Students student = new Students("S123", "John", "Doe", 20);
+        System.out.println("Student Information:");
+        student.getInfo();
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        // Создаем экземпляр класса Employee и вызываем getInfo
+        Employee employee = new Employee("Jane", "Smith", 45);
+        System.out.println("\nEmployee Information:");
+        employee.getInfo();
         }
     }
-}
